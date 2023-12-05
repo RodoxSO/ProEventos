2d2216c82c35b2fd09a771d01732854c9732e432
Rodolfo <63265096+RodoxSO@users.noreply.github.com>
2023-12-05 01:50:28 +0000

    first commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EventoController : ControllerBase
+{
+    private readonly DataContext _context;
+    public EventoController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+        return _context.Eventos;
+    }
+
+    [HttpGet("{id}")]
+    public Evento GetById(int id)
+    {
+        return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+    }
+
+    [HttpPost]
+    public string Post()
+    {
+        return "Exemplo de post";
+    }
+
+    [HttpPut("{id}")]
+    public string Put(int id)
+    {
+        return $"Exemplo de put com id = {id}";
+    }
+
+    [HttpDelete("{id}")]
+    public string Delete(int id)
+    {
+        return $"Exemplo de delete com id = {id}";
+    }
+}
